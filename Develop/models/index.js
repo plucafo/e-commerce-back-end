054b2bf87aaae5f678e0cc6b11b967a02ffd0ad2
plucafo <plucaf87@gmail.com>
2024-03-25 12:33:40 -0700

    set up models and model associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: 'category_id' });
 
 // Categories have many Products
+Category.hasMany(Product, { foreignKey: 'category_id' }); // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' }); // Products belong to many Tags through ProductTag
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' }); // Tags belong to many Products through ProductTag
 
 module.exports = {
   Product,
